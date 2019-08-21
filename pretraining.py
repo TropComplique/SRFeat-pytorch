db2f4a06dbdc5b7c3e167fdcf156fe1fcdb8bd21
TropComplique <antoshchenko.b@gmail.com>
2019-08-21 13:37:22 +0300

    tested

--- pretraining.py
+++ pretraining.py
@@ -4,8 +4,9 @@ import torch.nn.init as init
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.optim.lr_scheduler import LambdaLR
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 from input_pipeline import Images
 from generator import Generator
@@ -14,12 +15,12 @@ from torch.backends import cudnn
 cudnn.benchmark = True
 
 
-TRAIN_DATA = '/mnt/COCO/images/train2017'
-VALIDATION_DATA = '/mnt/COCO/images/val2017'
+TRAIN_DATA = '/mnt/COCO/images/train2017_only_big/'
+VALIDATION_DATA = '/mnt/COCO/images/val2017_only_big/'
 
 BATCH_SIZE = 8
 NUM_EPOCHS = 20
-SIZE = (296, 296)  # height and width
+SIZE = 296
 
 DEVICE = torch.device('cuda:0')
 MODEL_NAME = 'models/run00'
@@ -43,13 +44,8 @@ class Model:
         G = Generator(depth=128, num_blocks=16)
         self.G = G.apply(weights_init).to(device)
 
-        def lambda_rule(i):
-            decay = num_steps // 2
-            m = 1.0 if i < decay else 1.0 - (i - decay) / decay
-            return max(m, 0.0)
-
-        self.optimizer = optim.Adam(self.G.parameters(), lr=2e-4, betas=(0.5, 0.999))
-        self.scheduler = LambdaLR(self.optimizer, lr_lambda=lambda_rule)
+        self.optimizer = optim.Adam(self.G.parameters(), lr=1e-4, betas=(0.5, 0.999))
+        self.scheduler = CosineAnnealingLR(self.optimizer, num_steps, eta_min=1e-6)
         self.loss = nn.MSELoss()
 
         # a copy for exponential moving average
@@ -97,8 +93,8 @@ def accumulate(model_accumulator, model, decay=0.999):
 
 def downsample(images):
     return F.interpolate(
-        images, scale_factor=4,
-        mode='bilinear',
+        images, mode='bilinear', 
+        size=(SIZE // 4, SIZE // 4),
         align_corners=False
     )
 
@@ -126,6 +122,9 @@ def evaluate(model, val_loader):
 
 
 def main():
+    
+    # tensorboard --logdir=summaries/ --port=6007
+    writer = SummaryWriter(log_dir='summaries/')
 
     train_dataset = Images(TRAIN_DATA, SIZE, is_training=True)
     val_dataset = Images(VALIDATION_DATA, SIZE, is_training=False)
@@ -142,7 +141,7 @@ def main():
     val_loader = DataLoader(
         dataset=val_dataset, batch_size=32,
         shuffle=False, num_workers=4,
-        pin_memory=True, drop_last=True
+        pin_memory=False, drop_last=True
     )
 
     # number of weight updates
@@ -157,15 +156,18 @@ def main():
             i += 1
             loss = model.train_step(A, B)
 
-            print(e, i, round(loss, 4))
+            print(f'epoch {e}, iteration {i}, loss {round(loss, 4)}')
+            writer.add_scalars('loss', {'train': loss}, i)
 
         if e % EVAL_EPOCH == 0:
             loss = evaluate(model, val_loader)
             print('validation loss', round(loss, 4))
+            writer.add_scalars('loss', {'val': loss}, i)
 
         if e % SAVE_EPOCH == 0:
             model.save_model(MODEL_NAME + f'_epoch_{e}')
 
+    writer.close()
 
 if __name__ == '__main__':
     main()
